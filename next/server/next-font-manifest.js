139f6e388ffb22c5387cfcca3bb7e5056cb613dd
realfortis <b202035945@gmail.com>
2024-12-28 16:06:46 -0330

    rm junk

--- next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/app/layout\":[\"static/media/76ba26f12b5e00f7-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
